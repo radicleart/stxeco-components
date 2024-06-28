230442a7745dd62ed246c4c3549a33bf802af4ab
Mike Cohen <mjoecohen@gmail.com>
2024-06-28 17:16:45 +0100

    broke down api and added more common code.

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -4,7 +4,9 @@ import StxEcoHeader from "./header/StxEcoHeader.svelte";
 import StxEcoBrand from "./header/StxEcoBrand.svelte";
 import StxEcoAccountDropdown from "./header/StxEcoAccountDropdown.svelte";
 import StxEcoFooter from "./header/StxEcoFooter.svelte";
+import Placeholder from "./common/Placeholder.svelte";
 
+export { Placeholder };
 export { StxEcoHeader };
 export { StxEcoBrand };
 export { StxEcoAccountDropdown };
